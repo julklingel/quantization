1fb332c8520dce4ec18c4a204a3606422d651c2e
julklingel <julklingel@gmail.com>
2025-01-24 14:20:19 +0100

    data clean up & exploration

--- scripts/quantization_methods.py
+++ scripts/quantization_methods.py
@@ -2,16 +2,13 @@ def quantize_model(model, quantization_method):
     if quantization_method == 'post_training_static':
         pass
     elif quantization_method == 'post_training_dynamic':
-
         pass
     elif quantization_method == 'quantization_aware_training':
-
         pass
     else:
         raise ValueError("Unsupported quantization method")
 
 def evaluate_quantization(model, quantized_model, dataset):
-
     pass
 
 def main():
